9b5d2be8f0316cf84e0d02b543f912df57e93cc0
Deji Obasan <dejiobasan@yahoo.com>
2023-09-02 10:39:41 +0100

    .

--- my-portfolio-app/src/Components/About.jsx
+++ my-portfolio-app/src/Components/About.jsx
@@ -11,15 +11,16 @@ function About() {
             <h1 className="header2">About me</h1>
             <p>
               I'm a Software Engineer with years of experience building
-              web-based applications. I hold a First class (Honours) Bachelor’s
-              Degree in Electrical/Electronics Engineering. Additionally, I am
-              proficient in HTML, CSS (Bootstrap), JavaScript, Material UI, and
-              Reactjs for frontend development, and have experience with Nodejs,
-              Expressjs and Python for backend development. I am a skilled
-              full-stack developer and open to opportunities that would enable
-              me work on diverse projects and support the vision of the company
-              to deliver exceptional service without compromising standards. I'm
-              confident, hardworking and very open to learning.
+              web-based applications and in my spare time i watch alot of Anime.
+              I hold a First class (Honours) Bachelor’s Degree in
+              Electrical/Electronics Engineering. Additionally, I am proficient
+              in HTML, CSS (Bootstrap), JavaScript, Material UI, and Reactjs for
+              frontend development, and have experience with Nodejs, Expressjs
+              and Python for backend development. I am a skilled full-stack
+              developer and open to opportunities that would enable me work on
+              diverse projects and support the vision of the company to deliver
+              exceptional service without compromising standards. I'm confident,
+              hardworking and very open to learning.
             </p>
           </Fade>
         </div>
